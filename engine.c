3fe5c677cf92013157fa4b0e3528777f4238c8c0
Kousuke Takeuchi <u651601f@gmail.com>
2013-05-31 16:09:40 +0900

    Initial Game Engine
    敵の位置情報などを初期化する関数とデバッグようのメインルーチンを試作しました。まだgccを通せていないで、次回までにデバッグ＆改良しようと思いま
    す。

--- engine.c
+++ engine.c
@@ -2,21 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define HEIGHT 1200
-#define WIDTH 800
+#define FIELD_WIDTH 800
+#define FIELD_HEIGHT 1200
+#define ENEMY_NUM 10
 
-typedef struct {
+struct enemy {
 	int x, y;
-	int enemyNum;
+	int num;
 	bool isDestroyed;
-} enemy
+} 
 
 int main(void) {
-	unsigned int delay = 1;
-	sleep(delay);
+	struct enemy* enemies;
+	init(enemies);
+	int i = 0;
+	for (i=0; i<ENEMY_NUM-1; i++) {
+		printf("enemy[%s]: point(%d,%d) and %s",
+									 	i, enemies[i].x, enemies[i].y, enemies[i].isDestroyed ? "destroyed" : "alive");
+	}
 	return 0;
 }
 
-int init(void) {
-		
+int init(struct enemy* enemies) {
+	int enemy_width = FIELD_WIDTH / ENEMY_NUM;
+	int enemy_height = FIELD_WIDTH / ENEMY_NUM;
+	
+	int i;
+	for (i=1; i<ENEMY_NUM; i++)	{
+		enemies[i-1] = {enemy_width*(i-0.5), FIELD_HEIGHT-enemy_height/2, i, false};
+	}		
+	return 0;
 }
