e6470bc6be3d93e9e83e9015b875cf51022cd13d
Kousuke Takeuchi <u651601f@gmail.com>
2013-05-31 15:39:16 +0900

    Initial Commit by Kosuke Takeuchi
    ゲーム内の敵のオブジェクト、敵座標の初期化の実装を始めました。

---
+++ engine.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEIGHT 1200
+#define WIDTH 800
+
+typedef struct {
+	int x, y;
+	int enemyNum;
+	bool isDestroyed;
+} enemy
+
+int main(void) {
+	unsigned int delay = 1;
+	sleep(delay);
+	return 0;
+}
+
+int init(void) {
+		
+}
